bbf325d68ecdf3f7b2bb508d850c3c2cd46915cf
klimantovich <91698270+klimantovich@users.noreply.github.com>
2024-03-27 14:35:36 +0300

    feature-1 (#8)
    * feature-1
    ---------
    
    Co-authored-by: vitali <vitali@gmail.com>

--- terraform/01-providers.tf
+++ terraform/01-providers.tf
@@ -21,7 +21,7 @@ provider "helm" {
   }
 }
 
-# provider "kubectl" {
-#   host                   = data.aws_eks_cluster.eks.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority.0.data)
-# }
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+}
